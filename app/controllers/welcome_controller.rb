e711041b5ca9bc43450a72d574cbb33949065c2e
Heather Creighton <heather.creighton@gmail.com>
2016-03-19 09:35:56 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+  @meetings = Meeting.all
+end
+
+  def about
+  end
+end
---
+++ app/views/meetings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @meeting, :id, :name, :start_time, :end_time, :username, :created_at, :updated_at
